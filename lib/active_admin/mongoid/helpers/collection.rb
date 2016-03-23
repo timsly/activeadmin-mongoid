e652a6543180da171014f36fa2bdffc55f3450d9
Tim Maslyuchenko <insside@gmail.com>
2016-03-23 19:08:18 +0200

    mongoid 5

--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -5,15 +5,7 @@ module ActiveAdmin
       alias original_collection_size collection_size
       original_collection_size = instance_method(:collection_size)
       def collection_size(collection=collection)
-        if(not collection.empty? and collection.first.class.included_modules.include?(Mongoid::Document))
-          if collection.first.class.embedded?
-            collection.count
-          else
-            collection.count(true)
-          end
-        else
-          collection.count
-        end
+        collection.count
       end
     end
   end
