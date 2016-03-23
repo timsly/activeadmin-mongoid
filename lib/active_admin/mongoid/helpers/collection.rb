69bb4a50bcf357146fb4b5c8b99614197e97538f
Tim Maslyuchenko <insside@gmail.com>
2016-03-23 19:33:26 +0200

    mongoid 5

--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -4,8 +4,8 @@ module ActiveAdmin
 
       alias original_collection_size collection_size
       original_collection_size = instance_method(:collection_size)
-      def collection_size(collection=collection)
-        collection.count
+      def collection_size(c=collection)
+        c.count
       end
     end
   end
